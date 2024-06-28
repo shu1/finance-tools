e58fc93a63c543de3129c58015da275dacae9d1a
Shuichi Aizawa <saizawa@gmail.com>
2024-06-29 03:29:01 +0900

    Code cleanup

--- popup.js
+++ popup.js
@@ -16,8 +16,8 @@ function shusFinanceTools(mode) {
           if (res.result) {
             pre_post.innerHTML = res.result[0];
             change_sum.innerHTML = "$" + res.result[1];
-            chrome.action.setBadgeText({text: res.result[2].toFixed(1) + "%"});
-            chrome.action.setTitle({title: res.result[0] + "-Mkt Chg Sum: $" + res.result[1]});
+            chrome.action.setBadgeText({ text: res.result[2].toFixed(1) + "%" });
+            chrome.action.setTitle({ title: res.result[0] + "-Mkt Chg Sum: $" + res.result[1] });
           }
         });
     }
@@ -30,34 +30,7 @@ function contentScript(mode) {
   }
 
   const stockTable = document.querySelector("#pf-detail-table");
-  if (mode == 2) {
-    const prices = stockTable.querySelectorAll("[data-field=regularMarketPrice][data-trend=none]");
-    const highs = stockTable.querySelectorAll("[aria-label='52-Wk High']");
-    const lows = stockTable.querySelectorAll("[aria-label='52-Wk Low']");
-
-    const rangeArray = [];
-    for (let i = 0; i < prices.length; ++i) {
-      const price = Number(prices[i].getAttribute("value"));
-      const high = Number(highs[i].innerText.replace(/,/g, ""));
-      const low = Number(lows[i].innerText.replace(/,/g, ""));
-      rangeArray.push({
-        index: i,
-        symbol: prices[i].dataset.symbol,
-        percentage: (price - low) / (high - low),
-      });
-    }
-    rangeArray.sort((a, b) => a.percentage - b.percentage);
-
-    const rangeTable = {};
-    for (let i = 0; i < rangeArray.length; ++i) {
-      rangeTable[rangeArray[i].symbol] = {
-        change: i - rangeArray[i].index,
-        "52-Wk percentage": format(rangeArray[i].percentage * 100),
-      };
-    }
-    console.table(rangeTable);
-  }
-  else if (mode <= 1) {
+  if (mode <= 1) {
     const stockList = stockTable.querySelectorAll(".Fz\\(s\\)[data-field=regularMarketPrice][data-trend=none]");
 
     const p = ["Pre", "Post"];
@@ -93,5 +66,32 @@ function contentScript(mode) {
     const sumPercent = format(changeSum / portfolioSum * 100);
     console.log(`${p[mode]}-Mkt Chg Sum: $${format(changeSum)}, ${sumPercent}%`);
     return [p[mode], format(changeSum), sumPercent];
+  }
+  else if (mode == 2) {
+    const prices = stockTable.querySelectorAll("[data-field=regularMarketPrice][data-trend=none]");
+    const highs = stockTable.querySelectorAll("[aria-label='52-Wk High']");
+    const lows = stockTable.querySelectorAll("[aria-label='52-Wk Low']");
+
+    const rangeArray = [];
+    for (let i = 0; i < prices.length; ++i) {
+      const price = Number(prices[i].getAttribute("value"));
+      const high = Number(highs[i].innerText.replace(/,/g, ""));
+      const low = Number(lows[i].innerText.replace(/,/g, ""));
+      rangeArray.push({
+        index: i,
+        symbol: prices[i].dataset.symbol,
+        percentage: (price - low) / (high - low),
+      });
+    }
+    rangeArray.sort((a, b) => a.percentage - b.percentage);
+
+    const rangeTable = {};
+    for (let i = 0; i < rangeArray.length; ++i) {
+      rangeTable[rangeArray[i].symbol] = {
+        change: i - rangeArray[i].index,
+        "52-Wk percentage": format(rangeArray[i].percentage * 100),
+      };
+    }
+    console.table(rangeTable);
   }
 }
